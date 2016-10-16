4abfb0f3d53a13817fc77f2d6d05204c75c61b41
Sam Berry <samjamesberry@gmail.com>
2016-10-16 21:24:30 +0100

    Add files via upload

--- MovieAdvertProcessor.ps1
+++ MovieAdvertProcessor.ps1
@@ -1,15 +1,17 @@
 ﻿#DVR Processor Script
 #Compares contents of folder which is the output of MCEBuddy against the original file.
 #If the files video length are significantly different implying adverts have been removed, moves the new video to Movies folder for Plex and deletes original file from DVR.
-#Requires FFMPEG
-$DVRMoviesPath = "M:\DVR Movies\";
-$DVRProcessorPath = "M:\DVR Processor\";
-$FFMPEGPath = "D:\Users\Sam\Desktop\ffmpeg\bin\";
-$MoviesPath="M:\Movies\May Contain Adverts\";
-
+$DVRMoviesPath = "M:\DVR Movies\";#Path of folder that records DVR Movies
+$DVRProcessorPath = "M:\DVR Processor\";#Path of oflder that MCEBuddy outputs to
+$FFMPEGPath = "D:\Users\Sam\Desktop\ffmpeg\bin\";#Path of folder containning FFMEPG.exe
+$MoviesPath="M:\Movies\May Contain Adverts\";#Path to copy movies to after
 
 
 $file=Get-ChildItem $DVRProcessorPath -Filter *.mkv -File|% {$_.BaseName}|Select -first 1; #Get filename
+if($file -eq $null){
+echo "No file found";
+exit;
+}
 $newTime = (& ${FFMPEGPath}"ffprobe.exe" -i ${DVRProcessorPath}${file}".mkv" -show_entries format=duration -v quiet -of csv="p=0") | Out-String;#Get time of encoded file
 $oldTime = (& ${FFMPEGPath}"ffprobe.exe" -i ${DVRMoviesPath}${file}"\"${file}".ts" -show_entries format=duration -v quiet -of csv="p=0") | Out-String;#Get time of unencoded file
 $timeDifference = $oldTime-$newTime;#Find difference in time
