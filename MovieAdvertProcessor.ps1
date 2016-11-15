e7916385c5f8d3864b9527c94539c30623fac023
Sam Berry <samjamesberry@gmail.com>
2016-11-15 21:45:37 +0000

    Add files via upload

--- MovieAdvertProcessor.ps1
+++ MovieAdvertProcessor.ps1
@@ -8,7 +8,12 @@ $MoviesPath="M:\Movies\May Contain Adverts\";#Path to copy movies to after
 $TimeDifference = 60;
 
 
+try {
 $file=Get-ChildItem $DVRProcessorPath -Filter *.mkv -File|% {$_.BaseName}|Select -first 1; #Get filename
+}catch{
+echo "Path not found";
+exit;
+}
 if($file -eq $null){
 echo "No file found";
 exit;
@@ -16,15 +21,14 @@ exit;
 $newTime = (& ${FFMPEGPath}"ffprobe.exe" -i ${DVRProcessorPath}${file}".mkv" -show_entries format=duration -v quiet -of csv="p=0") | Out-String;#Get time of encoded file
 $oldTime = (& ${FFMPEGPath}"ffprobe.exe" -i ${DVRMoviesPath}${file}"\"${file}".ts" -show_entries format=duration -v quiet -of csv="p=0") | Out-String;#Get time of unencoded file
 $timeDifference = $oldTime-$newTime;#Find difference in time
-if ($timeDifference -gt $TimeDifference){
+if ($timeDifference -gt 60){
 Move-Item "${DVRProcessorPath}${file}.mkv" "${MoviesPath}";#Move new file to Movies library
 Remove-Item "${DVRMoviesPath}${file}" -Recurse;#Remove original file
 echo "Adverts removed for ${file}";
-}else
-{
+}else{
 Move-Item "${DVRProcessorPath}${file}.mkv" "${DVRProcessorPath}Manual Check\";#Move to extra folder for manual checking
-if($oldTime -eq $null){
-echo "Original file not found";
+    if($oldTime -eq $null){
+    echo "Original file not found";
 }
 echo "Manual checking required for ${file}";
 }
